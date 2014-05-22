16dbaffdf67d8aa23c12e001eb02a86528a21cdb
farmstyle <farmstyle1@gmail.com>
2014-05-22 13:50:46 +0700

    add file welcome.php and user_m.php

---
+++ controllers/welcome.php
@@ -0,0 +1,70 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+	class Welcome extends CI_Controller {
+
+			function __construct()
+			{
+				parent::__construct();
+				//	 โหลด model User	
+				//	โหลดไลบารี session และ form_validation
+				$this->load->model('User_m','user');					
+				$this->load->library(array( 'session', 'form_validation')); 
+				$this->load->helper(array('url', 'html', 'form'));
+			}
+			
+			
+			public function index()
+			{
+				$this->load->view('login_v');	
+			
+			}
+	
+			public function login(){  								
+				//	 ฟังก์ชั่น login
+				// 	เช็คค่าจากที่ส่งมาจาก input  		
+				if($post = $this->input->post()){ 					 
+				extract($post);					
+				//	 เรียกใช้ฟังก์ชัน _pubSelectUser จาก model User โดยสร้างตัวแปร $userid ในการรับค่า
+				$userid= $this->user->_pubSelectUser($user,$pass);	
+					if($userid){														
+										
+						$this->session->set_userdata('id',$userid->userid); 	
+						
+						// 	ใช้งาน load view				
+						$this->load->view('head_v');
+						$this->load->view('body_v');
+						$this->load->view('foot_v');
+					}else{echo "Login Fail";
+						$this->load->view('login_v');
+					}
+				}
+										
+			}
+			
+			public function regis()				
+			{	
+				//	 ฟังก์ชัน register ทำการสมัครข้อมูลมูล userid
+				if($post = $this->input->post()){ 				 
+				extract($post);				
+				// 	เรียกใช้ฟังก์ชัน _pubaddUser จาก model User โดยสร้างตัวแปร $userid ในการรับค่า
+				$this->user->_pubAddUser($userid,$password,$name,$email);	
+				redirect(base_url());
+			}
+			}
+			
+			public function logout()				
+			{
+				// ฟังก์ชันทำการ logout และล้าง session
+				$this->session->sess_destroy();		
+				redirect(base_url());
+			}
+		
+		
+	}
+		
+		
+	
+	
+	
+
+
---
+++ models/user_m.php
@@ -0,0 +1,36 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+		// 	มีหน้าที่ select, insert ข้อมูล user ของ customer
+		// 	เกี่ยวข้องกับ ตาราง user
+		// 	เกี่ยวข้องกับ ไฟล์ welcomec
+		// 	เขียนวันที่ 21-05-14
+	Class User_m extends CI_Model
+	{
+		function __construct()
+			{
+				parent::__construct();
+			
+			}
+ 
+		public function _pubSelectUser($user,$pass)		// select ข้อมูลจาก user
+		{	
+			// 	เงื่อนไขในการ select โดยกำหนดเป็นตัวแปล array
+			
+			$cause = array('userid'=>$user,'password'=>$pass);	
+			$query = $this->db->get_where('user',$cause);	
+			
+			// return ค่าแบบ row 
+			if($query -> num_rows() == 1){return $query->row();}
+			else{return false;}
+		}
+		
+		public function _pubAddUser($userid,$password,$name,$email)		// ฟังก์ชัน insert ข้อมูลลงฐานข้อมูล
+		{
+			$cause = array('userid'=>$userid,'password'=>$password,'name'=>$name,'email'=>$email);	
+			$this->db->insert('user',$cause);	// ดึงข้อมูลจาก member แบบมีเงื่อนไข
+
+			
+			return;						
+			
+		
+		}
+	}
