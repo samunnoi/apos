8e169eec99417d9e449e151f7e27a32250b37183
farmstyle <farmstyle1@gmail.com>
2014-05-22 17:00:26 +0700

    enter 1 line welcome.php

--- controllers/welcome.php
+++ controllers/welcome.php
@@ -1,68 +1,67 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 	class Welcome extends CI_Controller {
-		// หน้าจอ welcome  มีหน้าที่ เข้าสู่ระบบ หรือ ลงทะเบียนผู้ใช้งานเบื้องต้น
-		// เกี่ยวข้องกับ ตาราง user
-		// เกี่ยวข้องกับ ไฟล์ welcome.php user_m.php login_v.php
-		// เรียก Model ชื่อ user_m 
-		// เรียก view ชื่อ login_v
-		//  เขียน    วันที่ 21 -5 57
-		function __construct()
-		{
-			parent::__construct();
-			//	 โหลด model User	
-			//	โหลดไลบารี session และ form_validation
-			$this->load->model('User_m','user');					
-			$this->load->library(array( 'session', 'form_validation')); 
-			$this->load->helper(array('url', 'html', 'form'));
-		}
-		
-		
-		public function index()
-		{
-			$this->load->view('login_v');				
-		}
 
-		public function login(){  								
-			//	 ฟังก์ชั่น login
-			// 	เช็คค่าจากที่ส่งมาจาก input  		
-			if($post = $this->input->post()){ 					 
+	
+	
+			function __construct()
+			{
+				parent::__construct();
+				//	 โหลด model User	
+				//	โหลดไลบารี session และ form_validation
+				$this->load->model('User_m','user');					
+				$this->load->library(array( 'session', 'form_validation')); 
+				$this->load->helper(array('url', 'html', 'form'));
+			}
+			
+			
+			public function index()
+			{
+				$this->load->view('login_v');	
+			
+			}
+	
+			public function login(){  								
+				//	 ฟังก์ชั่น login
+				// 	เช็คค่าจากที่ส่งมาจาก input  		
+				if($post = $this->input->post()){ 					 
 				extract($post);					
 				//	 เรียกใช้ฟังก์ชัน _pubSelectUser จาก model User โดยสร้างตัวแปร $userid ในการรับค่า
 				$userid= $this->user->_pubSelectUser($user,$pass);	
-				if($userid){														
-									
-					$this->session->set_userdata('id',$userid->userid); 	
-					
-					// 	ใช้งาน load view				
-					$this->load->view('head_v');
-					$this->load->view('body_v');
-					$this->load->view('foot_v');
-					}else{
-					echo "Login Fail";
-					$this->load->view('login_v',);
+					if($userid){														
+										
+						$this->session->set_userdata('id',$userid->userid); 	
+						
+						// 	ใช้งาน load view				
+						$this->load->view('head_v');
+						$this->load->view('body_v');
+						$this->load->view('foot_v');
+					}else{echo "Login Fail";
+						$this->load->view('login_v');
+					}
 				}
+										
 			}
-									
-		}
+			
+			public function regis()				
+			{	
+				//	 ฟังก์ชัน register ทำการสมัครข้อมูลมูล userid
+				if($post = $this->input->post()){ 				 
+				extract($post);				
+				// 	เรียกใช้ฟังก์ชัน _pubaddUser จาก model User โดยสร้างตัวแปร $userid ในการรับค่า
+				$this->user->_pubAddUser($userid,$password,$name,$email);	
+				redirect(base_url());
+			}
+			}
+			
+			public function logout()				
+			{
+				// ฟังก์ชันทำการ logout และล้าง session
+				$this->session->sess_destroy();		
+				redirect(base_url());
+			}
 		
-		public function regis()				
-		{	
-			//	 ฟังก์ชัน register ทำการสมัครข้อมูลมูล userid
-			if($post = $this->input->post()){ 				 
-			extract($post);				
-			// 	เรียกใช้ฟังก์ชัน _pubaddUser จาก model User โดยสร้างตัวแปร $userid ในการรับค่า
-			$this->user->_pubAddUser($userid,$password,$name,$email);	
-			redirect(base_url());
-		}
-		}
 		
-		public function logout()				
-		{
-			// ฟังก์ชันทำการ logout และล้าง session
-			$this->session->sess_destroy();		
-			redirect(base_url());
-		}		
 	}
 		
 		
